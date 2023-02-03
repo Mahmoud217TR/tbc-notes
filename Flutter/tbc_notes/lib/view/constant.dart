f0b024a4fb8542c7699e5216738931553c3ec67d
LoulouTBC <loulouibrahim21@gmail.com>
2023-02-03 21:08:57 +0300

    Add homeScreen

--- Flutter/tbc_notes/lib/view/constant.dart
+++ Flutter/tbc_notes/lib/view/constant.dart
@@ -1,3 +1,7 @@
 import 'package:flutter/animation.dart';
 
-Color primerColor = Color(0xff333C4A);
+Color primerColor = const Color(0xff333C4A);
+Color primerColorWithOpacity = Color.fromARGB(77, 51, 60, 74);
+Color secondColor = const Color(0xff495664);
+Color thirdColor = const Color(0xffF6F7D3);
+Color fourthColor = const Color(0xffF8FCEB);
